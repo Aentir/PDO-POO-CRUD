07c413be368ea8aa5482cfdb871225bd3899d832
kenny Callado <kennycallado@hotmail.com>
2021-10-06 18:43:52 +0200

    kenny - 1   - Separa lógica get y show en clase Select. - Limpia contenido en nuevo showInfo:   - No necesita if()   - Mucha de la lógica se repetía

--- clases/Select.php
+++ clases/Select.php
@@ -10,72 +10,64 @@ class Select extends Connection
 
   public function getInfo($value = null)
   {
-    
-    //Si getInfo() recibe algún parametro (GET/POST) se lo pasará al IF, sino, entrará en el ELSE
+    /* array que devolverá el método */
+    $getArray = [];
+    /* consulta a la base de datos */
+    $query = "SELECT alum_dni, alum_nombre, alum_apellido1, alum_apellido2, alum_nota FROM t_alumnos ";
+    /* Prepara la consulta */
+
+    /* Sí existe un value se concatena a la consulta */
     if ($value) {
-      //Creo la query que quiero lanzar
-      $sql = "SELECT alum_dni, alum_nombre, alum_apellido1, alum_apellido2, alum_nota FROM t_alumnos WHERE alum_nombre = ?";
-
-      //prepare() lo que hace es preparar una sentencia para su ejecución y devuelve un obj
-      $result = $this->db->prepare($sql);
-
-      //execute() ejecuta la sentencia preparada con los valores que le hayamos pasado
-      $result->execute(array($value));
-
-      //Como lo que se devuelve es un objeto con forma de array
-      //lo recorro con un foreach ENTERO
-      $output = "";
-      foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $row) {
-        /*echo $row["alum_nombre"] . " " . $row["alum_apellido1"] . " " .
-          $row["alum_apellido2"] . " " . $row["alum_dni"] . "<br>";*/
-
-          $output .= "<tr>";
-          $output .= "<td>" . $row["alum_dni"] . "</td>";
-          $output .= "<td>" . $row["alum_nombre"] . "</td>";
-          $output .= "<td>" . $row["alum_apellido1"] . "</td>";
-          $output .= "<td>" . $row["alum_apellido2"] . "</td>";
-
-          //Este if comprueba que si la posición del array que recorremos (consulta) contiene la condición, se le aplicarán los cambios
-          //Lo mismo en el ELSE
-          if($row["alum_nota"] < 4) {
-            $output .= "<td style='background-color:salmon'>" . $row["alum_nota"] . "</td>";
-          } else {
-            $output .= "<td>" . $row["alum_nota"] . "</td>";
-          }
-          $output .= "<td><a href='deleteAlumn.php?id=" . $row["alum_dni"] . "'>Borrar alumno</a></td>";
-          $output .= "<td><a href='formUpdate.php?alum_dni=" . $row["alum_dni"] . "&name=". $row["alum_nombre"] . "&firstname=" 
-          . $row["alum_apellido1"] . "&firstname=" . $row["alum_apellido2"] . "&nota=". $row["alum_nota"] ."'>Actualizar alumno</a></td>";
-          $output .= "</tr>";
-      }
-    
+      $query .= "WHERE alum_dni = ?;";
     } else {
-      $query = "SELECT alum_dni, alum_nombre, alum_apellido1, alum_apellido2, alum_nota FROM t_alumnos";
-      $result = $this->db->prepare($query);
-      $result->execute(array());
-      $output = "";
-      foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $row) {
-        /*echo $row["alum_nombre"] . " " . $row["alum_apellido1"] . " " .
-          $row["alum_apellido2"] . " " . $row["alum_dni"] . "<br>";*/
-
-          $output .= "<tr>";
-          $output .= "<td>" . $row["alum_dni"] . "</td>";
-          $output .= "<td>" . $row["alum_nombre"] . "</td>";
-          $output .= "<td>" . $row["alum_apellido1"] . "</td>";
-          $output .= "<td>" . $row["alum_apellido2"] . "</td>";
-          if($row["alum_nota"] < 4) {
-            $output .= "<td style='background-color:salmon'>" . $row["alum_nota"] . "</td>";
-          } else {
-            $output .= "<td>" . $row["alum_nota"] . "</td>";
-          }
-          $output .= "<td><a href='deleteAlumn.php?alum_dni=" . $row["alum_dni"] . "'>Borrar alumno</a></td>";
-          /*$output .= "<td><a href='formUpdate.php?alum_dni=" . $row["alum_dni"] . "&name=". $row["alum_nombre"] . "&firstname=" 
-          . $row["alum_apellido1"] . "&firstname=" . $row["alum_apellido2"] . "&nota=". $row["alum_nota"] ."'>Actualizar alumno</a></td>";*/
-          $output .= "<td><a href='formUpdate.php?alum_dni=" . $row["alum_dni"] . "'>Actualizar alumno</a></td>";
-          $output .= "</tr>";
-      }   
+      $query .= ";";
     }
-    //var_dump($output);
+
+    /* Prepara y ejecuta la consulta  */
+    $result = $this->db->prepare($query);
+    $result->execute(array($value));
+
+    /* Guarda en array el resultado de la consulta */
+    foreach ($result->fetchAll(PDO::FETCH_ASSOC) as $row) {
+      array_push($getArray, $row);
+    }
+
+
+    return $getArray;
+  } // getInfo()
+
+  public function showInfo($value = null)
+  {
+    $output = "";
+    /* Recibe el resultado de la consulta getInfo */
+    $alumnos = $this->getInfo($value);
+
+    /* Por cada registro recibido crea una fila */
+    /* sirve para múltiples registros, un registro */
+    /* en vista debe tratarse el caso 0 registros */
+    foreach ($alumnos as $alumno) {
+      /* inicia row */
+      $output .= "<tr>";
+
+      $output .= "<td>" . $alumno["alum_dni"] . "</td>";
+      $output .= "<td>" . $alumno["alum_nombre"] . "</td>";
+      $output .= "<td>" . $alumno["alum_apellido1"] . "</td>";
+      $output .= "<td>" . $alumno["alum_apellido2"] . "</td>";
+
+      if ($alumno["alum_nota"] < 4) {
+        $output .= "<td style='background-color:salmon'>" . $alumno["alum_nota"] . "</td>";
+      } else {
+        $output .= "<td>" . $alumno["alum_nota"] . "</td>";
+      }
+
+      $output .= "<td><a href='deleteAlumn.php?id=" . $alumno["alum_dni"] . "'>Borrar alumno</a></td>";
+      $output .= "<td><a href='formUpdate.php?alum_dni=" . $alumno["alum_dni"] . "&name=" . $alumno["alum_nombre"] . "&firstname=" . $alumno["alum_apellido1"] . "&firstname=" . $alumno["alum_apellido2"] . "&nota=" . $alumno["alum_nota"] . "'>Actualizar alumno</a></td>";
+
+      /* cierra row */
+      $output .= "</tr>";
+    }
+
     return $output;
-  }
+  } // showInfo()
 }
-?>
+
